904053a19a39124646a31a904aacc7bea0e6ce6e
J10C3G7 <jrodcg@gmail.com>
2021-11-09 20:30:46 -0800

    Made changes to the method chain and processed a .csv file.

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import datetime
+# import datetime
 
-def load_process_csgo(path):
+def load_process_csgo(path, my_dict):
     #load data
     df1 = (
         pd.read_csv(path, encoding='latin')
@@ -16,12 +16,18 @@ def load_process_csgo(path):
         .assign(day='1')
     )
     df3 = (
-        df2.assign(date = pd.to_datetime(df2['year'].astype(str)  + df2['month'] + df2['day'], format='%Y%B%d'))
+        df2.assign(date = pd.to_datetime(df2['year'].astype(str)  + df2['month'] + df2['day'], format='%Y%B%d').dt.date)
         .drop(['year', 'month', 'day', 'peak', 'avg_peak_perc', 'gamename'], axis = 1)
         .sort_values('date')
         .assign(rolling_gain = df2['gain'].rolling(7, center=True).mean())
         .dropna()
+        .assign(release_Dates='')
     )
+    print(df3.dtypes)
 
-    df3 = df3[['date', 'avg', 'gain', 'rolling_gain']]
+    for x, y in my_dict.items():
+        for z in y:
+            df3.loc[df3['date'] == pd.to_datetime(z, format='%Y%m%d'), 'release_Dates'] += (x+"/")
+
+    df3 = df3[['date', 'avg', 'gain', 'rolling_gain', 'release_Dates']]
     return df3
