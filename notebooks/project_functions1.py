8b9b18911a22a20394ca610d3af82880073b87c0
J10C3G7 <jrodcg@gmail.com>
2021-11-24 01:56:32 -0800

    Added my Dashboard.

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -11,21 +11,26 @@ def load_process_csgo(path, my_dict):
     #process data
     df2 = (
         df1.loc[df1['gamename'].str.startswith("Counter-Strike: Global Offensive")]
-    #         df1
         .convert_dtypes()
         .assign(day='1')
     )
     df3 = (
         df2.assign(date = pd.to_datetime(df2['year'].astype(str)  + df2['month'] + df2['day'], format='%Y%B%d').dt.date)
         .drop(['year', 'month', 'day', 'peak', 'avg_peak_perc', 'gamename'], axis = 1)
-        .sort_values('date')
-        .assign(rolling_gain = df2['gain'].rolling(7, center=True).mean())
-        .dropna()
-        .assign(release_Dates='')
+        .sort_values('date', ignore_index = True)
+        
     )
+    df4 = (
+        df3.assign(rolling_avg = df3['avg'].rolling(7, center=True).mean())
+        .assign(rolling_gain = df3['gain'].rolling(4).mean())
+        .dropna()
+        .assign(release_Dates='No Releases')
+        .reset_index()
+    )
+    #load in markers
     for x, y in my_dict.items():
         for z in y:
-            df3.loc[df3['date'] == pd.to_datetime(z, format='%Y%m%d'), 'release_Dates'] += (x+"/")
+            df4.loc[df4['date'] == pd.to_datetime(z, format='%Y%m%d'), 'release_Dates'] = (x)
 
-    df3 = df3[['date', 'avg', 'gain', 'rolling_gain', 'release_Dates']]
-    return df3
+    df4 = df4[['date', 'avg', 'rolling_avg', 'gain', 'rolling_gain', 'release_Dates']]
+    return df4
